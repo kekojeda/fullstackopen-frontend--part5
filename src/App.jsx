bf6996ae1f120b4bccea9a11a25777b0578ec9ff
kekojeda <kekojeda@gmail.com>
2024-05-16 16:07:21 -0300

    Component login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import { Loginform } from './components/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -14,6 +15,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <Loginform/>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
